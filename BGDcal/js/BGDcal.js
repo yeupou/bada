62e6b5e104bba95c0249c1c3a16c2916b97a6013
Klink <klink@bender>
2011-07-07 14:11:57 +0200

    Update

--- BGDcal/js/BGDcal.js
+++ BGDcal/js/BGDcal.js
@@ -3,4 +3,31 @@ function testLogo()
  alert('Hello Bada');
 }
  
+
+
+var MonthsOfYear = new Array(12);
+	MonthsOfYear[0] = ("janvier");
+	MonthsOfYear[1] = ("février");
+	MonthsOfYear[2] = ("mars");
+	MonthsOfYear[3] = ("avril");
+	MonthsOfYear[4] = ("mai");
+	MonthsOfYear[5] = ("juin");
+	MonthsOfYear[6] = ("juillet");
+	MonthsOfYear[7] = ("août");
+	MonthsOfYear[8] = ("septembre");
+	MonthsOfYear[9] = ("octobre");
+	MonthsOfYear[10] = ("novembre");
+	MonthsOfYear[11] = ("décembre");
+	
+var DaysOfWeek = new Array(7);
+	DaysOfWeek[0] = ("lundi");
+	DaysOfWeek[1] = ("mardi");
+	DaysOfWeek[2] = ("mercredi");
+	DaysOfWeek[3] = ("jeudi");
+	DaysOfWeek[4] = ("vendredi");
+	DaysOfWeek[5] = ("samedi");
+	DaysOfWeek[6] = ("dimanche");
+
+
+
 // EOF
